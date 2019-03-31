7ca09e6137351fbb44757aa4e4ada037fa48144a
Marcelo Sercheli <msercheli@gmail.com>
2019-03-31 15:40:11 -0400

    building glew from url live download

---
+++ package.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+name = 'glew'
+
+version = '2.1.0'
+
+authors = ['Marcelo Sercheli']
+
+def commands():
+    env.LD_LIBRARY_PATH.prepend("{root}/lib64")
+    if building:
+        env.CMAKE_MODULE_PATH.append("{root}/cmake")
+
